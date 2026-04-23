fc8a29947d656791ad5b440182bc42ca81ef9c3f
motan <go68kib@tum.de>
2026-04-23 17:00:52 +0200

    initial working version of tissue specific filtering

---
+++ bin/tissue_specific_filtering.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import argparse
+from pathlib import Path
+import sys
+import pandas as pd 
+import graph_tool.all as gt 
+import util as utils 
+
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser(description="filter network by tissue specific expression")
+    parser.add_argument("--network",
+                        type=str,
+                        required=True,
+                        help="path to the network file in gt format")
+    parser.add_argument("--tissues",
+                        type=lambda x: x.split(','),
+                        required=True,
+                        help="tissues to filter by")
+    parser.add_argument("--threshold",
+                        type=float,
+                        default=1.0,
+                        help="expression threshold to filter by")
+    parser.add_argument("--expression_file",
+                        type=str,
+                        required=True,
+                        help="path to the expression file")
+    return parser.parse_args(argv)
+
+def filter_network_by_tissue(network, stem, tissues, expression_file, threshold):
+    name_index = utils.name2index(network)
+    expression_by_tissue = pd.read_csv(expression_file, sep="\t", skiprows=2, header = 0)
+    expression_by_tissue["Name"] = expression_by_tissue["Name"].apply(lambda x: x.split(".")[0])
+    in_network_genes = expression_by_tissue[expression_by_tissue["Name"].isin(name_index.keys())].copy()
+    in_network_genes["vertex_id"] = in_network_genes["Name"].map(name_index)
+    for tissue in tissues:
+        tissue_specific_genes = in_network_genes[in_network_genes[tissue] > threshold]
+        tissue_filter = network.new_vertex_property("bool")
+        tissue_filter.a[tissue_specific_genes["vertex_id"]] = True
+        network.set_vertex_filter(tissue_filter)
+        tissue_specific_graph = gt.GraphView(network, vfilt=tissue_filter)
+        tissue_specific_graph.save(f"{stem}_{tissue}_specific.gt")
+
+
+
+def main(argv=None):
+    args = parse_args(argv)
+    network = gt.load_graph(args.network)
+    stem = Path(args.network).stem
+    filter_network_by_tissue(network, stem,args.tissues, args.expression_file, args.threshold)
+
+if __name__ == "__main__":
+    sys.exit(main())
--- workflows/diseasemodulediscovery.nf
+++ workflows/diseasemodulediscovery.nf
@@ -21,7 +21,7 @@ include { MODULEOVERLAP                     } from '../modules/local/moduleoverl
 include { DRUGPREDICTIONS                   } from '../modules/local/drugpredictions/main'
 include { TOPOLOGY                          } from '../modules/local/topology/main'
 include { DRUGSTONEEXPORT                   } from '../modules/local/drugstoneexport/main'
-
+include { TISSUE_SPECIFIC_FILTERING         } from '../modules/local/tissue_specific_filtering/main'
 //
 // SUBWORKFLOW: Consisting of a mix of local and nf-core/modules
 //
@@ -146,8 +146,31 @@ workflow DISEASEMODULEDISCOVERY {
         )
     ch_multiqc_files = ch_multiqc_files.mix(ch_network_multiqc)
     ch_network_gt = GRAPHTOOLPARSER.out.network
-
-
+    if (params.tissues != null){
+        TISSUE_SPECIFIC_FILTERING(ch_network_gt,
+                                 "$projectDir/assets/expression_by_tissue.gct",
+                                 params.tissues
+                                 )
+        ch_versions = ch_versions.mix(TISSUE_SPECIFIC_FILTERING.out.versions)
+        ch_filtered_networks = TISSUE_SPECIFIC_FILTERING.out.filtered_networks
+            .flatMap { network_meta, tissue_files ->
+                tissue_files.collect { tissue_file ->
+            // extract tissue name from filename, e.g. "string..._Bladder_specific.gt" -> "Bladder_specific"
+                    def tissue = tissue_file.baseName.replace(network_meta.network_id + '_', '')
+                    def new_id = "${network_meta.network_id}_${tissue}"
+                    def new_meta = [id: new_id, network_id: new_id]
+                    [ new_meta, tissue_file ]
+                }
+            }
+        ch_network_gt = ch_network_gt.mix(ch_filtered_networks)
+        ch_seeds = ch_seeds
+            .combine(ch_network_gt.map{meta, _network -> meta.network_id})
+            .map{ meta, seeds, network_id -> 
+                def new_id = meta.seeds_id + "." + network_id
+                def new_meta = [id: new_id, seeds_id: meta.seeds_id, network_id: network_id]
+                [new_meta, seeds]
+            }
+    }
     // Check input
     // channel: [ val(meta[id,seeds_id,network_id]), path(seeds), path(network) ]
     ch_seeds_network = ch_seeds
