54ed99c0a36a890039dfd2fdc57928da6578ad5c
motan <go68kib@tum.de>
2026-05-13 11:39:52 +0200

    changes to filtering statisics to be more network centered

--- bin/tissue_specific_filtering.py
+++ bin/tissue_specific_filtering.py
@@ -35,7 +35,7 @@ def parse_args(argv=None):
 def convert_id_space(expression_df, id_space):
     ids = expression_df["Name"]
     if id_space == "ensembl": 
-        return expression_df, 0
+        return expression_df
     elif id_space == "entrez":
         targetSpace = "ENTREZGENE_ACC"
     elif id_space == "uniprot":
@@ -44,7 +44,7 @@ def convert_id_space(expression_df, id_space):
         raise ValueError(f"Unsupported id_space: {id_space}")
     gp = GProfiler(return_dataframe=True)
     query_result = gp.convert(organism="hsapiens", query=ids.tolist(),target_namespace=targetSpace)
-    not_found = query_result.loc[query_result["converted"].astype(str) == "None", "incoming"].shape[0] / expression_df.shape[0]
+    #not_found = query_result.loc[query_result["converted"].astype(str) == "None", "incoming"].shape[0] / expression_df.shape[0]
     query_result = query_result.merge(expression_df, left_on="incoming", right_on="Name")
     query_result = query_result[["incoming", "converted", "expression"]]
     collapsed_result = (
@@ -57,7 +57,7 @@ def convert_id_space(expression_df, id_space):
         })
     )
     collapsed_result = collapsed_result.rename(columns={"converted": "Name"})
-    return collapsed_result, not_found
+    return collapsed_result
 
 
 
@@ -73,13 +73,14 @@ def filter_network(network_file, threshold, expression_by_tissue, tissue):
     network.vp[tissue_filter] = network.new_vertex_property("bool")
     for vertex_id in in_network_tissue_genes["vertex_id"].unique():
         network.vp[tissue_filter][vertex_id] = True
+    in_Network_filtered_genes = network.num_vertices() - len(in_network_tissue_genes["vertex_id"].unique())
     network.set_vertex_filter(network.vp[tissue_filter])
     network.purge_vertices()
     network.clear_filters()
     del network.vp[tissue_filter]
     network.save(f"{stem}.{tissue}.gt")
     
-    return tissue_specific_genes.shape[0], not_in_network
+    return tissue_specific_genes.shape[0], not_in_network, in_Network_filtered_genes
 
 
 
@@ -94,11 +95,11 @@ def main(argv=None):
     expression_by_tissue = expression_by_tissue[["Name", args.tissue]]
     expression_by_tissue = expression_by_tissue.rename(columns={args.tissue: "expression"})
     #map gene_ids to the specified id space 
-    expression_by_tissue, n_not_mapped = convert_id_space(expression_by_tissue, args.id_space)
+    expression_by_tissue = convert_id_space(expression_by_tissue, args.id_space)
     #filter network by tissue specific expression with the given threshold 
-    n_genes_in_tissue, n_not_in_network =filter_network(args.network ,args.threshold, expression_by_tissue, args.tissue)
+    n_genes_in_tissue, n_not_in_network, in_Network_filtered_genes = filter_network(args.network ,args.threshold, expression_by_tissue, args.tissue)
     with open("filtering_statistic.tsv", "w") as f:
-        f.write("tissue\tpercent_genes_not_mapped\tgenes_in_tissue\tgenes_filtered_out\n")
-        f.write(f"<a href=https://gtexportal.org/home/tissue/{args.tissue}>{args.tissue}</a>\t{n_not_mapped}\t{n_genes_in_tissue}\t{n_not_in_network}\n")
+        f.write("tissue\tgenes_in_tissue\tgenes_not_in_network\tin_network_filtered_genes\n")
+        f.write(f"<a href=https://gtexportal.org/home/tissue/{args.tissue}>{args.tissue}</a>\t{n_genes_in_tissue}\t{n_not_in_network}\t{in_Network_filtered_genes}\n")
 if __name__ == "__main__":
     sys.exit(main())
